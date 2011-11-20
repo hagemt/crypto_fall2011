dd5e7b0d9f8cc1443414c5db0e9b13feb68911e8
Tor E Hagemann <hagemt@rpi.edu>
2011-11-20 03:44:07 -0500

    Structured ATM session data and added preliminary command code.

--- atm.c
+++ atm.c
@@ -16,38 +16,114 @@
 #include "banking_commands.h"
 #include "socket_utils.h"
 
+struct client_session_data_t {
+  int sock;
+  int key;
+  // TODO? char buffer[MAX_COMMAND_LENGTH];
+} session_data;
+
 int
-login_command(char * cmd)
+authenticated(struct client_session_data_t * session_data) {
+  char response_buffer[MAX_COMMAND_LENGTH];
+  send(session_data->sock, AUTH_CHECK_MSG, sizeof(AUTH_CHECK_MSG), 0);
+  recv(session_data->sock, response_buffer, MAX_COMMAND_LENGTH, 0);
+  response_buffer[MAX_COMMAND_LENGTH - 1] = '\0';
+  #ifndef NDEBUG
+  fprintf(stderr, "INFO: recv '%s'\n", response_buffer);
+  #endif
+  memset(response_buffer, 0, MAX_COMMAND_LENGTH);
+  /* TODO perform actual authentication */
+  if (session_data->key) {
+    return BANKING_OK;
+  }
+  return BANKING_ERROR;
+}
+
+int
+login_command(const char * cmd)
 {
-  printf("Recieved command: %s\n", cmd);
+  char PIN_buffer[MAX_COMMAND_LENGTH];
+
+  if (authenticated(&session_data)) {
+    /* TODO better way to fetch PIN? */
+    printf("Enter PIN: ");
+    read(0, PIN_buffer, MAX_COMMAND_LENGTH);
+    PIN_buffer[MAX_COMMAND_LENGTH - 1] = '\0';
+
+    /* TODO perform actual authorization */
+    if (authenticated(&session_data)) {
+      printf("AUTHENTICATION FAILURE\n");
+    }
+  } else {
+    printf("You must 'logout' first.\n");
+  }
+
+  memset(PIN_buffer, 0, MAX_COMMAND_LENGTH);
   return BANKING_OK;
 }
 
 int
-balance_command(char * cmd)
+balance_command(const char * cmd)
 {
-  printf("Recieved command: %s\n", cmd);
+  size_t len;
+  char response_buffer[MAX_COMMAND_LENGTH];
+  if (authenticated(&session_data)) {
+    len = strnlen(cmd, MAX_COMMAND_LENGTH);
+    send(session_data.sock, cmd, len, 0);
+    recv(session_data.sock, response_buffer, MAX_COMMAND_LENGTH, 0);
+    response_buffer[MAX_COMMAND_LENGTH - 1] = '\0';
+    printf("%s\n", response_buffer);
+  }
+  memset(response_buffer, 0, MAX_COMMAND_LENGTH);
   return BANKING_OK;
 }
 
 int
-withdraw_command(char * cmd)
+withdraw_command(const char * cmd)
 {
-  printf("Recieved command: %s\n", cmd);
+  size_t len;
+  char response_buffer[MAX_COMMAND_LENGTH];
+  if (authenticated(&session_data)) {
+    len = strnlen(cmd, MAX_COMMAND_LENGTH);
+    send(session_data.sock, cmd, len, 0);
+    recv(session_data.sock, response_buffer, MAX_COMMAND_LENGTH, 0);
+    response_buffer[MAX_COMMAND_LENGTH - 1] = '\0';
+    printf("%s\n", response_buffer);
+  }
+  memset(response_buffer, 0, MAX_COMMAND_LENGTH);
   return BANKING_OK;
 }
 
 int
-logout_command(char * cmd)
+logout_command(const char * cmd)
 {
-  printf("Recieved command: %s\n", cmd);
+  if (authenticated(&session_data)) {
+    printf("You did not 'login' first.\n");
+  } else {
+    /* TODO proper deauthorization */
+    session_data.key = 0;
+  }
+  /* Ensure we are not authenticated */
+  if (!authenticated(&session_data)) {
+    fprintf(stderr, "MISAUTHORIZATION DETECTED\n");
+    return BANKING_ERROR;
+  }
   return BANKING_OK;
 }
 
 int
-transfer_command(char * cmd)
+transfer_command(const char * cmd)
 {
-  printf("Recieved command: %s\n", cmd);
+  size_t len;
+  char response_buffer[MAX_COMMAND_LENGTH];
+  if (authenticated(&session_data)) {
+    len = strnlen(cmd, MAX_COMMAND_LENGTH);
+    send(session_data.sock, cmd, len, 0);
+    recv(session_data.sock, response_buffer, MAX_COMMAND_LENGTH, 0);
+    response_buffer[MAX_COMMAND_LENGTH - 1] = '\0';
+    printf("%s\n", response_buffer);
+  }
+  memset(response_buffer, 0, MAX_COMMAND_LENGTH);
   return BANKING_OK;
 }
 
@@ -56,7 +132,6 @@ main(int argc, char ** argv)
 {
   char * in;
   command cmd;
-  int csock;
   int caught_signal;
 
   /* Input sanitation */
@@ -64,13 +139,14 @@ main(int argc, char ** argv)
     fprintf(stderr, "USAGE: %s port_num\n", argv[0]);
     return EXIT_FAILURE;
   }
-  if ((csock = init_client_socket(argv[1])) < 0) {
+  if ((session_data.sock = init_client_socket(argv[1])) < 0) {
     fprintf(stderr, "FATAL: proxy unreachable\n");
     return EXIT_FAILURE;
   }
 
   /* Issue an interactive prompt, terminate only on failure */
   for (caught_signal = 0; !caught_signal && (in = readline(PROMPT));) {
+    session_data.key = !session_data.key;
     /* Read in a line, then attempt to associate it with a command */
     if (validate(in, &cmd)) {
       /* Ignore empty commands */
@@ -86,7 +162,7 @@ main(int argc, char ** argv)
     in = NULL;
   }
 
-  csock = destroy_socket(csock);
+  destroy_socket(session_data.sock);
   printf("\n");
   return EXIT_SUCCESS;
 }
