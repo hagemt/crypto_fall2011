a85999eded6500d3d91db01fd252a96a16b8cb0b
Tor E Hagemann <hagemt@rpi.edu>
2011-11-29 01:37:40 -0500

    Massive overhaul of entire database interaction after review

--- db_utils.h
+++ db_utils.h
@@ -25,6 +25,21 @@
 
 #include "banking_constants.h"
 
+struct account_info_t
+{
+  const char * name;
+  long int balance;
+  size_t length;
+};
+
+#define INIT_ACCOUNT(NAME, BALANCE) { #NAME, BALANCE, sizeof(#NAME) }
+
+const struct account_info_t accounts[] = {
+  INIT_ACCOUNT(Alice, 100),
+  INIT_ACCOUNT(Bob,    50),
+  INIT_ACCOUNT(Eve,     0)
+};
+
 void
 destroy_db(const char * db_path, sqlite3 * db_conn) {
   if (db_path && remove(db_path)) {
@@ -35,41 +50,118 @@ destroy_db(const char * db_path, sqlite3 * db_conn) {
   }
 }
 
-#define NUM_INIT_DB_CMDS 5
-#define INIT_DB_CMD_LEN 50
-const char initial_commands[NUM_INIT_DB_CMDS][INIT_DB_CMD_LEN] = {
-  "CREATE TABLE accounts(name, balance INTEGER);",
-  "INSERT INTO  accounts values('Alice',   100);",
-  "INSERT INTO  accounts values('Bob',      50);",
-  "INSERT INTO  accounts values('Eve',       0);",
-  "SELECT * FROM accounts;"
-};
-
 int
 init_db(const char * db_path, sqlite3 ** db_conn)
 {
-  int i; const char * residue;
+  size_t i;
+  const char * residue;
   sqlite3_stmt * statement;
+
   if(sqlite3_open(db_path, db_conn)) {
     fprintf(stderr, "ERROR: unable to open database\n");
     destroy_db(db_path, *db_conn);
     return BANKING_ERROR;
   }
+
   /* Create the table with preliminary data */
-  for (i = 0; i < NUM_INIT_DB_CMDS - 1; ++i) {
-    sqlite3_prepare(*db_conn, initial_commands[i], INIT_DB_CMD_LEN, &statement, &residue);
+  assert(sqlite3_prepare(*db_conn, SQL_CMD_CREATE_TABLE, sizeof(SQL_CMD_CREATE_TABLE), &statement, &residue) == SQLITE_OK);
+  assert(sqlite3_step(statement) == SQLITE_DONE);
+  assert(sqlite3_finalize(statement) == SQLITE_OK);
+  for (i = 0; i < sizeof(accounts) / sizeof(struct account_info_t); ++i) {
+    assert(sqlite3_prepare(*db_conn, SQL_CMD_INSERT_ACCOUNT, sizeof(SQL_CMD_INSERT_ACCOUNT), &statement, &residue) == SQLITE_OK);
+    assert(sqlite3_bind_text(statement, 1, accounts[i].name, accounts[i].length, SQLITE_STATIC) == SQLITE_OK);
+    assert(sqlite3_bind_int(statement, 2, accounts[i].balance) == SQLITE_OK);
     assert(sqlite3_step(statement) == SQLITE_DONE);
-    assert(sqlite3_reset(statement) == SQLITE_OK);
+    assert(sqlite3_finalize(statement) == SQLITE_OK);
   }
+
   #ifndef NDEBUG
-  sqlite3_prepare(*db_conn, initial_commands[i], INIT_DB_CMD_LEN, &statement, &residue);
+  assert(sqlite3_prepare(*db_conn, SQL_CMD_SELECT_ALL, sizeof(SQL_CMD_SELECT_ALL), &statement, &residue) == SQLITE_OK);
   fprintf(stderr, "Name\tBalance\n");
   while (sqlite3_step(statement) != SQLITE_DONE) {
-    residue = (const char *)sqlite3_column_text(statement, 0);
-    i = sqlite3_column_int(statement, 1);
-    fprintf(stderr, "%s\t%i\n", residue, i);
+    fprintf(stderr, "%s\t%i\n", sqlite3_column_text(statement, 0), sqlite3_column_int(statement, 1));
   }
+  assert(sqlite3_finalize(statement) == SQLITE_OK);
   #endif
+
+  return BANKING_OK;
+}
+
+int first_row;
+
+int select_callback(void *p_data, int num_fields, char **p_fields, char **p_col_names) {
+
+  int i;
+
+  int* nof_records = (int*) p_data;
+  (*nof_records)++;
+
+  if (first_row) {
+    first_row = 0;
+
+    for (i=0; i < num_fields; i++) {
+      printf("%20s", p_col_names[i]);
+    }
+
+    printf("\n");
+    for (i=0; i< num_fields*20; i++) {
+      printf("=");
+    }
+    printf("\n");
+  }
+
+  for(i=0; i < num_fields; i++) {
+    if (p_fields[i]) {
+      printf("%20s", p_fields[i]);
+    }
+    else {
+      printf("%20s", " ");
+    }
+  }
+
+  printf("\n");
+  return 0;
+}
+
+void select_stmt(sqlite3 * db, const char* stmt) {
+  char *errmsg;
+  int   ret;
+  int   nrecs = 0;
+
+  first_row = 1;
+
+  ret = sqlite3_exec(db, stmt, select_callback, &nrecs, &errmsg);
+
+  if(ret!=SQLITE_OK) {
+    printf("Error in select statement %s [%s].\n", stmt, errmsg);
+  }
+  else {
+    printf("\n   %d records returned.\n", nrecs);
+  }
+}
+
+int
+do_lookup(sqlite3 * db_conn, const char ** residue, char * name, size_t name_len, long int * balance)
+{
+  sqlite3_stmt * statement;
+  select_stmt(db_conn, SQL_CMD_SELECT_ALL);
+  assert(sqlite3_prepare(db_conn, SQL_CMD_LOOKUP_BALANCE, sizeof(SQL_CMD_LOOKUP_BALANCE), &statement, residue) == SQLITE_OK);
+  assert(sqlite3_bind_text(statement, 1, name, name_len, SQLITE_STATIC) == SQLITE_OK);
+  assert(sqlite3_step(statement) == SQLITE_DONE);
+  *balance = sqlite3_column_int(statement, 0);
+  assert(sqlite3_finalize(statement) == SQLITE_OK);
+  return BANKING_OK;
+}
+
+int
+do_update(sqlite3 * db_conn, const char ** residue, char * name, size_t name_len, long int balance)
+{
+  sqlite3_stmt * statement;
+  assert(sqlite3_prepare(db_conn, SQL_CMD_UPDATE_BALANCE, sizeof(SQL_CMD_UPDATE_BALANCE), &statement, residue) == SQLITE_OK);
+  assert(sqlite3_bind_int(statement, 1, balance) == SQLITE_OK);
+  assert(sqlite3_bind_text(statement, 2, name, name_len, SQLITE_STATIC) == SQLITE_OK);
+  assert(sqlite3_step(statement) == SQLITE_DONE);
+  /* TODO check for issues with integer overflow, final value? */
   assert(sqlite3_finalize(statement) == SQLITE_OK);
   return BANKING_OK;
 }
