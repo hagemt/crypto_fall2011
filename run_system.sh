3e97c113e0a80deb7ee5df1fdf8d5f078f83ce2c
Tor E Hagemann <hagemt@rpi.edu>
2012-01-01 23:39:07 -0600

    Fixed driver script to be directory-agnostic

--- run_system.sh
+++ run_system.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+BASE_DIR=$(dirname $0)
 TERM_CMD='gnome-terminal'
-TERM_CMD="$TERM_CMD --tab --title=\"BANK\"  --command=\"./build/bin/bank  49152\""
-TERM_CMD="$TERM_CMD --tab --title=\"PROXY\" --command=\"./build/bin/proxy 49153 49152\""
-TERM_CMD="$TERM_CMD --tab --title=\"ATM\"   --command=\"./build/bin/atm   49153\""
-bash -c "$TERM_CMD"
+TERM_CMD="${TERM_CMD} --tab --title=\"BANK\"  --command=\"${BASE_DIR}/build/bin/bank  49152\""
+TERM_CMD="${TERM_CMD} --tab --title=\"PROXY\" --command=\"${BASE_DIR}/build/bin/proxy 49153 49152\""
+TERM_CMD="${TERM_CMD} --tab --title=\"ATM\"   --command=\"${BASE_DIR}/build/bin/atm   49153\""
+bash -c "${TERM_CMD}"
