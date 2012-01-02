b23b6dfb614e2cb387edc2c0a5e594bb42ead27e
Tor E Hagemann <hagemt@rpi.edu>
2012-01-01 21:29:04 -0600

    Patched some silly issues with the proxy

--- proxy.c
+++ proxy.c
@@ -26,7 +26,7 @@
 /* Local includes */
 #include "socket_utils.h"
 
-struct proxy_session_data {
+struct proxy_session_data_t {
   int csock, ssock, conn;
   unsigned char buffer[MAX_COMMAND_LENGTH];
 } session_data;
@@ -38,7 +38,6 @@ handle_signal(int sig)
   fprintf(stderr, "INFO: stopping proxy service [signal %i]\n", sig);
   #endif
 
-  /* TODO flush buffers? */
   if (session_data.conn >= 0) {
     destroy_socket(session_data.conn);
   }
@@ -48,6 +47,9 @@ handle_signal(int sig)
   if (session_data.csock >= 0) {
     destroy_socket(session_data.csock);
   }
+
+  /* TODO needs to flush buffer? */
+  memset(session_data.buffer, 0, MAX_COMMAND_LENGTH);
 
   if (sig == SIGINT) {
     signal(SIGINT, SIG_DFL);
@@ -60,9 +62,7 @@ handle_connection(int sock, int * conn)
 {
   struct sockaddr_in addr_info;
   socklen_t addr_len = sizeof(addr_info);
-
   assert(sock >= 0 && conn);
-
   if ((*conn = accept(sock, (struct sockaddr *)&addr_info, &addr_len)) >= 0) {
     #ifndef NDEBUG
     fprintf(stderr, "INFO: tunnel established [%s:%hu]\n",
@@ -74,9 +74,9 @@ handle_connection(int sock, int * conn)
 }
 
 inline int
-handle_relay(struct proxy_session_data * session, ssize_t * r, ssize_t * s) {
-  assert(session && r && s);
-
+handle_relay(struct proxy_session_data_t * session, ssize_t * r, ssize_t * s) {
+  assert(session && session->buffer && r && s);
+  memset(session->buffer, 0, MAX_COMMAND_LENGTH);
   if ((*r = recv(session->conn,  session->buffer, MAX_COMMAND_LENGTH, 0)) > 0
    && (*s = send(session->csock, session->buffer, MAX_COMMAND_LENGTH, 0)) > 0) {
     return BANKING_SUCCESS;
@@ -110,13 +110,12 @@ main(int argc, char ** argv)
   while (!handle_connection(session_data.ssock, &session_data.conn)) {
     while (!handle_relay(&session_data, &received, &sent)) {
       if (sent != received) {
-        fprintf(stderr, "%li bytes lost!\n", (long)(received - sent));
-      #ifndef NDEBUG
-      } else {
-        session_data.buffer[MAX_COMMAND_LENGTH - 1] = '\0';
-        printf("FW: '%s'\n", session_data.buffer);
-      #endif
+        fprintf(stderr, "%li bytes lost!\n", (long)(sent - received));
       }
+      #ifndef NDEBUG
+      session_data.buffer[MAX_COMMAND_LENGTH - 1] = '\0';
+      printf("FW: '%s'\n", session_data.buffer);
+      #endif
     }
     destroy_socket(session_data.conn);
   }
