44ca91d69ee9192d11f74ca256ae7265eee91829
Tor E Hagemann <hagemt@rpi.edu>
2012-01-06 14:34:59 -0600

    Improvements to the proxy, should be final

--- proxy.c
+++ proxy.c
@@ -22,22 +22,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <time.h>
 
 /* Local includes */
 #include "socket_utils.h"
 
 struct proxy_session_data_t {
-  int csock, ssock, conn;
+  int csock, ssock, conn, count;
+  time_t established, terminated;
   unsigned char buffer[MAX_COMMAND_LENGTH];
 } session_data;
 
 void
 handle_signal(int signum)
 {
-  #ifndef NDEBUG
-  fprintf(stderr, "INFO: stopping proxy service [signal %i]\n", signum);
-  #endif
+  fprintf(stderr, "WARNING: stopping proxy service [signal %i: %s]\n", signum, strsignal(signum));
 
+  /* Attempt to disconnect from everything */
   if (session_data.conn >= 0) {
     destroy_socket(session_data.conn);
   }
@@ -48,28 +49,29 @@ handle_signal(int signum)
     destroy_socket(session_data.csock);
   }
 
-  /* TODO needs to flush buffer? */
+  /* Clear the internal buffer */
   memset(session_data.buffer, '\0', MAX_COMMAND_LENGTH);
 
-  if (signum == SIGINT) {
-    signal(SIGINT, SIG_DFL);
-    raise(SIGINT);
+  /* Re-raise the proper termination signals */
+  if (signum == SIGINT || signum == SIGTERM) {
+    signal(signum, SIG_DFL);
+    raise(signum);
   }
 }
 
-inline int
+int
 handle_connection(int sock, int * conn)
 {
-  char buffer[INET_ADDRSTRLEN];
+  char addr_buffer[INET_ADDRSTRLEN];
   struct sockaddr_in remote_addr;
   socklen_t addr_len = sizeof(remote_addr);
   assert(sock >= 0 && conn);
   if ((*conn = accept(sock, (struct sockaddr *)(&remote_addr), &addr_len)) >= 0) {
-    #ifndef NDEBUG
+    time(&session_data.established);
+    /* Report successful connection information */
     fprintf(stderr, "INFO: tunnel established [%s:%hu]\n",
-      inet_ntop(AF_INET, &remote_addr.sin_addr, buffer, INET_ADDRSTRLEN),
+      inet_ntop(AF_INET, &remote_addr.sin_addr, addr_buffer, INET_ADDRSTRLEN),
       ntohs(remote_addr.sin_port));
-    #endif
     return BANKING_SUCCESS;
   }
   return BANKING_FAILURE;
@@ -78,9 +80,11 @@ handle_connection(int sock, int * conn)
 inline int
 handle_relay(struct proxy_session_data_t * session, ssize_t * r, ssize_t * s) {
   assert(session && session->buffer && r && s);
+  /* Echo a single message */
   memset(session->buffer, '\0', MAX_COMMAND_LENGTH);
   if ((*r = recv(session->conn,  session->buffer, MAX_COMMAND_LENGTH, 0)) > 0
    && (*s = send(session->csock, session->buffer, MAX_COMMAND_LENGTH, 0)) > 0) {
+    ++session_data.count;
     return BANKING_SUCCESS;
   }
   return BANKING_FAILURE;
@@ -91,10 +95,12 @@ main(int argc, char ** argv)
 {
   ssize_t received, sent;
 
+  /* Input sanitation */
   if (argc != 3) {
     fprintf(stderr, "USAGE: %s listen_port bank_port\n", argv[0]);
     return EXIT_FAILURE;
   }
+
   /* Capture SIGINT and SIGTERM, TODO sigaction? */
   if (signal(SIGINT, &handle_signal) == SIG_IGN) {
     signal(SIGINT, SIG_IGN);
@@ -103,30 +109,39 @@ main(int argc, char ** argv)
     signal(SIGTERM, SIG_IGN);
   }
 
+  /* Socket initialization */
   if ((session_data.csock = init_client_socket(argv[2])) < 0) {
     fprintf(stderr, "ERROR: unable to connect to server\n");
     return EXIT_FAILURE;
   }
-
   if ((session_data.ssock = init_server_socket(argv[1])) < 0) {
     fprintf(stderr, "ERROR: unable to start server\n");
     destroy_socket(session_data.csock);
     return EXIT_FAILURE;
   }
 
+  /* Provide a dumb echo tunnel service */
   while (!handle_connection(session_data.ssock, &session_data.conn)) {
+    session_data.count = 0;
     while (!handle_relay(&session_data, &received, &sent)) {
+      /* Report leaky transmissions */
       if (sent != received) {
-        fprintf(stderr, "%li bytes lost!\n", (long)(sent - received));
+        fprintf(stderr, "ERROR: %li bytes lost\n", (long)(sent - received));
       }
       #ifndef NDEBUG
+      /* Report entire transmission */
       session_data.buffer[MAX_COMMAND_LENGTH - 1] = '\0';
       printf("FW: '%s'\n", session_data.buffer);
       #endif
     }
+    time(&session_data.terminated);
+    /* Disconnect from defunct clients */
     destroy_socket(session_data.conn);
+    fprintf(stderr, "INFO: tunnel closed [%i sent in %li sec]\n",
+      session_data.count, (long)(session_data.terminated - session_data.established));
   }
 
+  /* Teardown */
   handle_signal(0);
   return EXIT_SUCCESS;
 }
